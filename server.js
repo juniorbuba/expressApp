ddbb88508298cfab3beb235666d02d9c3bd37bb8
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2020-08-03 00:10:07 +0100

    starting point of application

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var config = require('./server/config');
 app = express();
+app = config(app);
 app.set('port', process.env.PORT || 6000);
 app.set('views', __dirname + '/views');
 
@@ -8,5 +10,5 @@ app.get('/', function(req,res){
 });
 
 app.listen(app.get('port'), function(){
-    console.log('Server is running');
+    console.log(`Server is running on port: ` + app.get('port'));
 });
