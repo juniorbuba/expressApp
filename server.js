0bbecd9ae76739480645e0f9ee8faba37ecde743
Buba Mohammed Auwal <juniorbuba4real@gmail.com>
2020-08-12 04:06:17 +0100

    commented out config file attachments in this file

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var config = require('./server/config');
+// var config = require('./server/config');
 app = express();
-app = config(app);
-app.set('port', process.env.PORT || 6000);
+// app = config(app);
+app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req,res){
